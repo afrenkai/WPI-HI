472e534073be96357442b198a1fa5bf9672a7b43
Artem <frenkartemcool1@gmail.com>
2025-03-14 23:27:36 -0400

    added funny regex to exclude weird dupe cols

--- create_ds.py
+++ create_ds.py
@@ -2,39 +2,61 @@
 from datasets import Dataset, DatasetDict, Features, Value, Image
 import os
 import pandas as pd
+from tqdm import tqdm
+import re
+
+def feat_type(dtype):
+    if pd.api.types.is_integer_dtype(dtype):
+        return Value("int32")
+    elif pd.api.types.is_float_dtype(dtype):
+        return Value("float32")
+    elif pd.api.types.is_bool_dtype(dtype):
+        return Value("bool")
+    else:
+        return Value("string")
 
 def create_ds():
     metadata_files = [os.path.join('Data/Metadata', file) for file in os.listdir('Data/Metadata') if file.endswith('.csv')]
-    print('metadata files made')
+    # print('metadata files made')
     image_files = sorted([os.path.join('Data/Image', file) for file in os.listdir('Data/Image') if file.endswith('.jpg')])
-    print('img files made')
+    # print('img files made')
 
     all_metadata = []
-    print('blank md list made')
-    for mdfile in metadata_files:
+    # print('blank md list made')
+    for mdfile in tqdm(metadata_files):
         sample_df = pd.read_csv(mdfile)
         sample_df.reset_index(drop = True, inplace = True)
         all_metadata.append(sample_df)
-        print(f'added {mdfile} to metadata df')
+        # print(f'added {mdfile} to metadata df')
     if not all_metadata:
         raise ValueError('404 md not found lol')
-
-    metadata_df = pd.concat(all_metadata, ignore_index=True).astype(str) #temp fix for pitch, need to fix later 
+    # print('all md dfs made')
+    metadata_df = pd.concat(tqdm(all_metadata), ignore_index=True) 
+    #print(metadata_df.isnull().sum())
+    # metadata_df = metadata_df.fillna('').astype(str)
     print('metadata df made')
-    ds = {'image': image_files  }
+    
+    ds = {'image': image_files}
+    good_col = []
+    for col in tqdm(metadata_df.columns):
+        # print(col)
+        col_comp = re.sub(r'([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?', r'', col)
+        # print(col_comp)
+        if col_comp not in ds.keys():
+            good_col.append(col)
+            ds[col] = metadata_df[col].tolist() 
+            print(f'added {col_comp}')
 
-    for col in metadata_df.columns:
-        ds[col] = metadata_df[col].tolist()
 
     
-    features = Features({
-        'image': Image(), 
-        **{col: Value('string') for col in metadata_df.columns} 
-    })
-  
-    dataset = Dataset.from_dict(ds, features=features) 
-    dataset_dict = DatasetDict({'train': dataset})
 
+    features = Features({'image': Image(),
+                **{col: feat_type(metadata_df[col]) for col in good_col}})
+
+    dataset = Dataset.from_dict(ds, features = features)
+    print(type(dataset['image']))
+
+    dataset_dict = DatasetDict({'train': dataset})
     dataset_dict.push_to_hub('afrenkai/WPI-Historical-Image-Collection')
 
     return dataset_dict
